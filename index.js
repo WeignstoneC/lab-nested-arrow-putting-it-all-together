449758e4e34c509b4ff2afbad4610c75fb263870
WeignstoneC <weignstonechurchill@gmail.com>
2026-04-04 00:14:03 +0300

    Add Login Tracker code

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+ let isLocked = false;
+    return (passwordAttempt) => {
+        if (isLocked) {
+            return "Account locked due to too many failed login attempts";
+        }
+        attemptCount++;
+        if (attemptCount > 3) {
+            isLocked = true;
+            return "Account locked due to too many failed login attempts";
+        }
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            return `Attempt ${attemptCount}: Login failed`;
+        }
+    };
+}
